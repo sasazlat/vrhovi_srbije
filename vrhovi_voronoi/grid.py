5ca123030df64a25c10ab331cc79b38018593e11
Саша Златковић <sashsoft@gmail.com>
2018-08-08 07:27:38 +0200

    voronoi - grid and edges

---
+++ vrhovi_voronoi/grid.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from bresenham import bresenham
+from scipy.spatial import Voronoi, voronoi_plot_2d
+
+def create_grid_edges(data, drone_altitude, safety_distance):
+    """
+    Returns a grid representation of a 2D configuration space
+    based on given obstacle data, drone altitude and safety distance
+    arguments.
+    """
+
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil(north_max - north_min))
+    east_size = int(np.ceil(east_max - east_min))
+
+    # Initialize an empty grid
+    grid = np.zeros([north_size, east_size])
+
+    #points for voronoi
+    points = []
+    # Populate the grid with obstacles
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+        if alt + d_alt + safety_distance > drone_altitude:
+            n_min = int(np.clip(north - d_north - safety_distance - north_min, 0, north_size - 1))
+            n_max = int(np.clip(north + d_north + safety_distance - north_min, 0, north_size - 1))
+            e_min = int(np.clip(east - d_east - safety_distance - east_min, 0, east_size - 1))
+            e_max = int(np.clip(east + d_east + safety_distance - east_min, 0, east_size - 1))
+
+            grid[n_min:n_max + 1, e_min:e_max + 1] = 1
+
+            points.append([north - north_min, east - east_min])
+
+    voronoi_graph = Voronoi(points)
+
+    #voronoi_plot_2d(graph)
+    #plt.show()
+    ridges = voronoi_graph.ridge_vertices
+    edges = []
+    for edge in ridges:
+        p1 = ridges[edge[0]]
+        p2 = ridges[edge[1]]
+
+        cells = list(bresenham(int(p1[0]),int(p1[1]),int(p2[0]),int(p2[1])))
+        hit = False
+        for cell in cells:
+            if np.amin(cell) < 0 or cell[0] >= grid.shape[0] or cell[1] >= grid.shape[1]:
+                hit = True
+                break
+            if grid[cell[0], cell[1]] == 1:
+                hit = True
+                break
+        if not hit:
+            p1 = (p1[0], p1[1])
+            p2 = (p2[0], p2[1])
+            edges.append((p1,p2))
+
+    return grid, edges
--- vrhovi_voronoi/vrhovi_voronoi.py
+++ vrhovi_voronoi/vrhovi_voronoi.py
@@ -1,1 +1,41 @@
+import numpy as np
+from grid import create_grid_edges
+import pickle
+
+#data = np.loadtxt('vrhovi_new.csv', delimiter=',',dtype='Float64')
+
+
+#print(data)
+
+#new_data = []
+#for row in data:
+#    if np.absolute(row[0]) > 1000 or np.absolute(row[1]) > 1000:
+#        continue
+#    else:
+#        new_data.append(row)
+
+#small_data = np.array(new_data)
+#print(small_data)
+
+#grid, edges = create_grid_edges(small_data, 10, 10)
+
+#grid_pickle_out = open("grid_p.pickel", mode='wb')
+#pickle.dump(grid, grid_pickle_out)
+#grid_pickle_out.close()
+
+
+#outfile = open("edges_p.pickel", mode='wb')
+#pickle.dump(edges, outfile)
+#outfile.close()
+
+in_grid = open("grid_p.pickel", mode='rb')
+grid = pickle.load(in_grid)
+in_grid.close()
+
+in_edges = open("edges_p.pickel", mode='rb')
+edges = pickle.load(in_edges)
+in_edges.close()
+
+print ('Fount %5d edges' % len(edges))
+
 
